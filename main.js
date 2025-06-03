88cf8de461eb9cfb8137c241af921bfe57e30c0e
Hrishikesh Tiwari <tiwarihrishikesh686@gmail.com>
2025-06-03 14:30:13 +0530

    crawler function ans site checks

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("hELLO world!");
+const { crawlPage } = require('./crawl.js'); 
+
+function main(){
+    if(process.argv.length < 3){
+        console.log("no website provided"); 
+        process.exit(1); // error code 
+    }
+
+    if(process.argv.length > 3){
+        console.log("too many command line args"); 
+        process.exit(1); 
+    }
+
+    const baseURL = process.argv[2]; 
+    // for(const arg of process.argv){
+    //     console.log(arg); 
+    // }
+
+    console.log(`starting crawl of ${baseURL}`); 
+    crawlPage(baseURL); 
+}
+
+main()
